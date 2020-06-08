e6fccd32bb2ea199ebbbf60e530bafa111fbc85b
Chen_1278 <604680830@qq.com>
2020-06-08 22:49:46 +0800

    实验

---
+++ Ex1_1.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Ex1_1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Ex1_1
+{
+     public static void main(String args[]){
+         int i=2;
+         int sum=0;
+         while(i<=100){
+             sum+=i;
+             i=i+2;
+            }
+            System.out.println(sum);
+        }
+    }
